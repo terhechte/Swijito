b50d9a0181b0b4278cd145de54f5b92662cd023b
Benedikt Terhechte <terhechte@gmail.com>
2014-06-09 01:39:56 +0200

    Initial Commit

---
+++ SwiftTestX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftTestX
+//
+//  Created by Benedikt Terhechte on 09/06/14.
+//  Copyright (c) 2014 Benedikt Terhechte. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftTestX/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftTestX
+//
+//  Created by Benedikt Terhechte on 09/06/14.
+//  Copyright (c) 2014 Benedikt Terhechte. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
