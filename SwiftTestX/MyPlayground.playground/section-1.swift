6bb55a29c20c1193f882ca579dd931da0b702a31
Benedikt Terhechte <terhechte@gmail.com>
2014-06-14 14:47:20 -0400

    Added a nice icon

--- SwiftTestX/MyPlayground.playground/section-1.swift
+++ SwiftTestX/MyPlayground.playground/section-1.swift
@@ -4,3 +4,19 @@ import Cocoa
 
 var str = "Hello, playground"
 
+func secretMethod() -> String? {
+    return "Yes"
+}
+
+var result: String? = secretMethod()
+switch result {
+case .None:
+    println("is nothing")
+case let a:
+    println("is a value")
+}
+
+
+
+
+
