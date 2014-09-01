2d663d61c89c1c7bd15091a7c4a8fa72fb946019
Benedikt Terhechte <terhechte@gmail.com>
2014-09-01 15:25:36 +0200

    Added functions file for simple extensions and abstractions

---
+++ SwiftTestX/UsefulFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  UsefulFunctions.swift
+//  Swijito
+//
+//  Created by Benedikt Terhechte on 9/1/14.
+//  Copyright (c) 2014 Benedikt Terhechte. All rights reserved.
+//
+
+import Foundation
